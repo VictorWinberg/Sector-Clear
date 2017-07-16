0b64c995dfa0834e8453b945f3cf62ac9aa4994c
Victor Winberg <dat14vwi@student.lu.se>
2017-07-16 17:11:27 +0200

    Integration with UnityGame part 1 (new files)

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameUI : MonoBehaviour {
+
+	public Image fadeCanvas;
+	public GameObject gameOverUI;
+
+	public RectTransform waveBanner;
+	public Text waveTitle, waveEnemyCount;
+
+	Spawner spawner;
+
+	void Awake() {
+		spawner = FindObjectOfType<Spawner> ();
+		spawner.OnNewWave += OnNewWave;
+	}
+
+	void Start () {
+		FindObjectOfType<Player> ().OnDeath += OnGameOver;
+	}
+
+	void OnNewWave(int waveNumber) {
+		waveTitle.text = "- Wave " + HumanFriendlyInteger.IntegerToWritten (waveNumber) + " -";
+		string enemyCount = (spawner.waves [waveNumber - 1].infinite) ? "Infinite" : spawner.waves [waveNumber - 1].enemyCount + "";
+		waveEnemyCount.text = "Enemies: " + enemyCount;
+		StopCoroutine ("AnimateWaveBanner");
+		StartCoroutine ("AnimateWaveBanner");
+	}
+
+	IEnumerator AnimateWaveBanner () {
+		float delayTime = 1.5f;
+		float speed = 3f;
+		float animatePercent = 0f;
+		int dir = 1;
+
+		float endDelayTime = Time.time + 1 / speed + delayTime;
+
+		while (animatePercent >= 0) {
+			animatePercent += Time.deltaTime * speed * dir;
+
+			if(animatePercent >= 1) {
+				animatePercent = 1;
+				if(Time.time > endDelayTime) {
+					dir = -1;
+				}
+			}
+
+			waveBanner.anchoredPosition = Vector2.up * Mathf.Lerp(-150, 150, animatePercent);
+			yield return null;
+		}
+	}
+	
+	void OnGameOver () {
+		StartCoroutine(Fade(Color.clear, Color.black, 1));
+		gameOverUI.SetActive (true);
+	}
+
+	IEnumerator Fade(Color from, Color to, float time) {
+		float speed = 1 / time;
+		float percent = 0;
+
+		while (percent < 1) {
+			percent += Time.deltaTime * speed;
+			fadeCanvas.color = Color.Lerp(from, to, percent);
+			yield return null;
+		}
+	}
+
+	// UI Input
+	public void StartNewGame() {
+		Application.LoadLevel ("Scene1");
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public bool developerMode;
+
+	public Wave[] waves;
+	public Enemy enemy;
+
+	LivingEntity player;
+
+	Wave currentWave;
+	int currentWaveNumber;
+
+	int enemiesRemainingToSpawn, enemiesRemainingAlive;
+	float nextSpawnTime;
+
+	MapGenerator map;
+
+	float idleTimeCheck = 2;
+	float idleThresholdDistance = 1.5f;
+	float nextIdleTimeCheck;
+	Vector3 idlePositionPrevious;
+	bool isIdle;
+
+	bool isDisabled;
+
+	public event System.Action<int> OnNewWave;
+
+	void Start () {
+		player = FindObjectOfType<Player> ();
+
+		nextIdleTimeCheck = idleTimeCheck + Time.time;
+		idlePositionPrevious = player.transform.position;
+		player.OnDeath += OnPlayerDeath;
+
+		map = FindObjectOfType<MapGenerator> ();
+		NextWave ();
+	}
+
+	void Update () {
+		if (isDisabled)
+			return;
+
+		if (Time.time > nextIdleTimeCheck) {
+			nextIdleTimeCheck = idleTimeCheck + Time.time;
+
+			isIdle = (Vector3.Distance(player.transform.position, idlePositionPrevious) < idleThresholdDistance);
+			idlePositionPrevious = player.transform.position;
+		}
+
+		if ((enemiesRemainingToSpawn > 0 || currentWave.infinite) && Time.time > nextSpawnTime) {
+			enemiesRemainingToSpawn--;
+			nextSpawnTime = Time.time + currentWave.timeBetweenSpawns;
+
+			StartCoroutine("SpawnEnemy");
+		}
+
+		if (developerMode) {
+			if(Input.GetKeyDown(KeyCode.Return)) {
+				StopCoroutine("SpawnEnemy");
+				foreach(Enemy enemy in FindObjectsOfType<Enemy>()) {
+					GameObject.Destroy(enemy.gameObject);
+				}
+				NextWave();
+			}
+		}
+	}
+
+	IEnumerator SpawnEnemy() {
+		float spawnDelay = 1;
+		float tileFlashSpeed = 4;
+
+		Transform spawnTile = isIdle ? map.getTileFromPosition(player.transform.position) : map.getRandomOpenTile ();
+		Material tileMaterial = spawnTile.GetComponent<Renderer> ().material;
+		Color initialColor = map.getInitialTileColor ();
+		Color flashColor = Color.red;
+		float spawnTimer = 0;
+
+		while (spawnTimer < spawnDelay) {
+
+			tileMaterial.color = Color.Lerp(initialColor, flashColor, Mathf.PingPong(spawnTimer * tileFlashSpeed, 1));
+
+			spawnTimer += Time.deltaTime;
+			yield return null;
+		}
+		tileMaterial.color = initialColor;
+		Enemy spawnedEnemy = Instantiate(enemy, spawnTile.position + Vector3.up, Quaternion.identity) as Enemy;
+		spawnedEnemy.OnDeath += OnEnemyDeath;
+
+		spawnedEnemy.SetCharacteristics (currentWave.moveSpeed, currentWave.damage, currentWave.health, currentWave.skinColor);
+	}
+
+	void OnPlayerDeath() {
+		isDisabled = true;
+	}
+
+	void OnEnemyDeath (){
+		enemiesRemainingAlive--;
+
+		if (enemiesRemainingAlive == 0) {
+			NextWave();
+		}
+	}
+
+	void ResetPlayerPosition() {
+		player.transform.position = map.getTileFromPosition(Vector3.zero).position + Vector3.up * 3;
+	}
+
+	void NextWave() {
+		currentWaveNumber++;
+		if (currentWaveNumber - 1 < waves.Length) {
+			currentWave = waves [currentWaveNumber - 1];
+			
+			enemiesRemainingToSpawn = currentWave.enemyCount;
+			enemiesRemainingAlive = enemiesRemainingToSpawn;
+
+			if(OnNewWave != null) OnNewWave(currentWaveNumber);
+			ResetPlayerPosition();
+		}
+	}
+
+	[System.Serializable]
+	public class Wave {
+		public bool infinite;
+		public int enemyCount;
+		public float timeBetweenSpawns;
+
+		public float moveSpeed;
+		public int damage;
+		public float health;
+		public Color skinColor;
+	}
+}
